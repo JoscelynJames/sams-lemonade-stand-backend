d041983779a4b15e875bd144129ab34af2b9739b
Joscelyn James <josce.james7@hotmail.com>
2018-01-15 19:15:51 -0700

    GraphQL API with insert mutations and get all queries

---
+++ src/data/address.js
@@ -0,0 +1,16 @@
+const Address = [
+	{
+		id: '8dlx7ak38fd39dv79ad',
+		address: '1Gr7LgKdyMZ3pGbmxFyXC2L61PpALokX7U',
+	},
+	{
+		id: 'jd3kd03d0w9a0l35rh74',
+		address: '1HzBaXgBZd9q8o7s6ZfEimQe6sxjtiJDC5',
+	},
+	{
+		id: '0hy894hf0dlkfh9oinv',
+		address: '1CVM9S4udKbpAAukTR5YMJi2FEqZHRdJdC',
+	}
+];
+
+module.exports = Address;
---
+++ src/data/transaction.js
@@ -0,0 +1,29 @@
+const Transactions = [
+	{
+		id: '8dlx7ak38fd39dv79ad',
+		amount: '3895894358',
+		address_id: '8dlx7ak38fd39dv79ad'
+	},
+	{
+		id: 'jd3kd03d0w9a0l35rh74',
+		amount: '49853903',
+		address_id: 'jd3kd03d0w9a0l35rh74'
+	},
+	{
+		id: '0hy894hf0dlkfh9oinv',
+		amount: '43045390',
+		address_id: '0hy894hf0dlkfh9oinv',
+	},
+	{
+		id: '0hy894hf0dlkfh9oinv',
+		amount: '1029385',
+		address_id: '0hy894hf0dlkfh9oinv',
+	},
+	{
+		id: '0hy894hf0dlkfh9oinv',
+		amount: '83472457',
+		address_id: '0hy894hf0dlkfh9oinv',
+	},
+];
+
+module.exports = Transactions;
